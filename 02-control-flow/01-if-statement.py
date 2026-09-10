7a54f1257b77452807b347502deb441136daeeb6
Owen Parker <oparker@worcester.edu>
2026-09-10 15:21:12 -0400

    added if statements

--- 02-control-flow/01-if-statement.py
+++ 02-control-flow/01-if-statement.py
@@ -1,1 +1,7 @@
-# Sometimes, we want to 
+# Sometimes, we want to run code only under certain conditions. If statements can help us with that
+
+print("How old are you?")
+age = int(input())
+
+if age < 21:
+    print("Sorry, you're too young to buy beer")
