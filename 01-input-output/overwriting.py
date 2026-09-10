4b440f317948e244c71847c3a69546c72c1ffc27
Owen Parker <oparker@worcester.edu>
2026-09-10 14:51:59 -0400

    wrote overwriting.py

---
+++ 01-input-output/overwriting.py
@@ -0,0 +1,9 @@
+# Variables can change their values, hence the name "variable". Before running this program, try to predict
+# what the output will be. Lines are run in order from top to bottom
+
+cat = "Cat in the Hat"
+cat = "Garfield"
+
+print(cat)
+
+cat = "Heathcliff"
