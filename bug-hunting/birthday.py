587042a7c8769205e44802557016c8017e0f6a4c
Owen Parker <oparker@worcester.edu>
2026-09-15 14:23:19 -0400

    Added comment to birthday.py

--- bug-hunting/birthday.py
+++ bug-hunting/birthday.py
@@ -1,17 +1,17 @@
+# There's one bug lurking...
+
 print("How many days are there until your birthday?")
 days = int(input())
 
-message = None
-
-if days > 100:
-    message = "It's nowhere near your birthday!"
-elif days > 30:
-    message = "Your day is coming up!"
+if days > 30:
+    message = "It's nowhere near your birthday"
 elif days > 5:
-    message = "I bet your friends are planning a party!"
+    message = "Not long now"
+elif days > 0:
+    message = "I bet your friends are planning a party"
 elif days = 0:
     message = "Happy birthday!"
 else:
-    message = "It seems your birthday has passed!"
+    message = "It seems your birthday has passed"
 
 print(message)
