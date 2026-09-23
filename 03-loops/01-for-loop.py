1eeb123315bdfa1f7e9a5a7c73eb632295e13e14
Owen Parker <oparker@worcester.edu>
2026-09-23 10:01:52 -0400

    Added loop examples

---
+++ 03-loops/01-for-loop.py
@@ -0,0 +1,6 @@
+# Here, we set `numbers` to the output of `range(10)`, which works out to [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = range(10)
+
+for i in numbers:
+    # The variable `i` only exists in here, and its value changes each iteration
+    print(i)
---
+++ 03-loops/02-while-loop.py
@@ -0,0 +1,6 @@
+a = 10
+
+# This will keep looping until the condition `a > 0` evaluates to `False`
+while a >= 0:
+    print(a)
+    a -= 1
